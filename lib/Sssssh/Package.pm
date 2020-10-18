2ce420832adcdee8a08a74c0b5a49135b7350b51
Ton Hospel <git@ton.iguana.be>
2020-10-19 00:23:18 +0200

    Fully working udp_proxy

--- lib/Sssssh/Package.pm
+++ lib/Sssssh/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "016";
-$release_time = 1602869009;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "017";
+$release_time = 1603059794;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
