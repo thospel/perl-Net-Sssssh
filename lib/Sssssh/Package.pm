6e3ef54d5bf4d0a1fe6c663a9e8034a252558ca7
Ton Hospel <git@ton.iguana.be>
2021-04-01 12:36:27 +0200

    Fix more fragments bug

--- lib/Sssssh/Package.pm
+++ lib/Sssssh/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "022";
-$release_time = 1617241963;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "023";
+$release_time = 1617273582;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
