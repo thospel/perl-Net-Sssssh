a52a83df123feb7f549f89043989f5f46b2a7276
Ton Hospel <git@ton.iguana.be>
2021-04-01 13:18:55 +0200

    Handle overly large FOU packets

--- lib/Sssssh/Package.pm
+++ lib/Sssssh/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "023";
-$release_time = 1617273582;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "024";
+$release_time = 1617275932;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
