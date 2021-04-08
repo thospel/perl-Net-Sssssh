01d966caae33c526bac3671af15371cdd445003f
Ton Hospel <git@ton.iguana.be>
2021-04-08 09:36:01 +0200

    Updates
    - Move settings into the LineProtocol object
    - Add an ack system

--- lib/Sssssh/Package.pm
+++ lib/Sssssh/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "026";
-$release_time = 1617660390;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "027";
+$release_time = 1617867354;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
