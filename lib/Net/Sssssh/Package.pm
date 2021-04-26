61cb0e3ecce149041806bd7dbe29b55d3180a44c
Ton Hospel <git@ton.iguana.be>
2021-04-27 00:21:09 +0200

    Initial signal handling. Based on perl signals, so it won't interrupt long running XS code

--- lib/Net/Sssssh/Package.pm
+++ lib/Net/Sssssh/Package.pm
@@ -6,8 +6,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "039";
-$release_time = 1619433890;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "040";
+$release_time = 1619512780;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
